99de16247a816b8134df3332e4372f8bf12694af
JosueLessie <joslessie@hotmail.com>
2023-10-09 19:52:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Variable Swap
+
+a = 5.00
+b = 10.50
+
+# Swap the values of a and b using a third variable
+temp = a
+a = b
+b = temp
+
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
